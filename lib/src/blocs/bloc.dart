a55cf9138e7c147ebe1c5e3e92240c7a24d45eb2
RafiaChy <rafiachowdhury24@gmail.com>
2021-12-09 22:12:05 +0600

    improved bloc api

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
 
 class Bloc {
-  final emailController = StreamController<String>();
-  final passawordController = StreamController<String>();
+  final _emailController = StreamController<String>();
+  final _passawordController = StreamController<String>();
+
+  Function(String) get addEmail => _emailController.sink.add;
+  Function(String) get addPassword => _passawordController.sink.add;
+
+  Stream<String> get email => _emailController.stream;
+  Stream<String> get password => _passawordController.stream;
 }
